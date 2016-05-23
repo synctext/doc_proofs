647e348077ceab75efbf4c2852a8504f557f817c
Ranga Krishnan <rk@selwastor.com>
2016-05-22 22:38:26 -0700

    Supersede to supersede

--- ProofTx/proofTx.pb.js
+++ ProofTx/proofTx.pb.js
@@ -55,7 +55,7 @@ module.exports = require("protobufjs").newBuilder({})['import']({
                 {
                     "rule": "optional",
                     "type": "SupersededBy",
-                    "name": "Supersede",
+                    "name": "supersede",
                     "id": 9
                 }
             ],
